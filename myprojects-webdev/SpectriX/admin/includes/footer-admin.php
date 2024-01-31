4e3f5465dbfa8bb76f8896080d9ffddec9e896cc
nisehtml <codearchvs@gmail.com>
2024-01-31 01:12:49 -0500

    Delete myprojects-webdev directory

--- myprojects-webdev/SpectriX/admin/includes/footer-admin.php
+++
@@ -1,4 +0,0 @@
-<div class="text-center p-3" style="background-color:#231f20; padding: 20px; color: #fff;">
-      © 2023 Copyright
-      <a style="color:#fff; hover:#425563;" href="../index.php">SpectriX</a>
-    </div>
